047f29a8b76d490f2d1722e15fbdfe8e662d4b59
songpanxiang1 <songpanxiang1@jd.com>
2022-03-21 18:09:32 +0800

    develop提交修改2

--- SearchHistory/app/src/main/java/cn/cnpp/searchhistory/MainActivity.java
+++ SearchHistory/app/src/main/java/cn/cnpp/searchhistory/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-//        这是测试数据1
+//        这是测试数据2
 
         initView();
         mediaplay.updateSongCount(100);
